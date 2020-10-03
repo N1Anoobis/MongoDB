9c5edaad1c480770d6b90c4891e861a89ef0b82d
N1Anoobis <mrbond84@hotmail.com>
2020-10-03 20:26:51 +0200

    Remove unnecessary code

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res) => {
 })
 
 const dbURI = process.env.NODE_ENV === 'production' ? 'mongodb+srv://slawomir:energy2000@cluster0.rqbyt.mongodb.net/companyDB?retryWrites=true&w=majority' : 'mongodb://localhost:27017/companyDB';	
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });	mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 
 db.once('open', () => {
